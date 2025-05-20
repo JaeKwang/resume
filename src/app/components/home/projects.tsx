156810523e09b37d6fc8cddc68d5ce7d42f2a92b
Park JaeKwang <sowkdxkd12@gmail.com>
2025-05-20 22:23:01 +0900

    Carousel 업데이트

--- src/app/components/home/projects.tsx
+++ src/app/components/home/projects.tsx
@@ -1,14 +1,18 @@
-'use client'
-import "@/app/globals.css"
-import Carousel from "./Carousel"
-import data from "@/data/projects.json"
+"use client";
+import "@/app/globals.css";
+import Carousel from "./Carousel";
+import data from "@/data/projects.json";
 
 export default function Projects() {
-
   return (
     <div className="px-6 py-24">
       <h2 className="text-4xl font-bold text-center mb-16">프로젝트</h2>
+      <h2 className="text-2xl font-bold text-center">[사내개발]</h2>
+      <Carousel items={data} />
+      <h2 className="text-2xl font-bold text-center">[외주개발]</h2>
+      <Carousel items={data} />
+      <h2 className="text-2xl font-bold text-center">[자체개발]</h2>
       <Carousel items={data} />
     </div>
-  )
+  );
 }
