05d709dddad7c064f8422a916e4f822f1bf1c1bb
jkpark125 <jkpark125@gmail.com>
2025-06-16 06:57:17 +0900

    프로젝트 순서 변경

--- src/app/components/home/projects.tsx
+++ src/app/components/home/projects.tsx
@@ -12,10 +12,10 @@ export default function Projects() {
       <p className="font-cookierun text-5xl sm:text-6xl md:text-7xl text-center mb-10 sm:mb-15 md:mb-20">프로젝트</p>
       <p className="font-cookierun text-2xl sm:text-3xl md:text-4xl text-center">[사내 프로젝트]</p>
       <Carousel items={ihData} />
-      <p className="font-cookierun text-2xl sm:text-3xl md:text-4xl text-center">[외주 개발]</p>
-      <Carousel items={cbDta} />
       <p className="font-cookierun text-2xl sm:text-3xl md:text-4xl text-center">[자체 기획 프로젝트]</p>
       <Carousel items={sdData} />
+      <p className="font-cookierun text-2xl sm:text-3xl md:text-4xl text-center">[외주 개발]</p>
+      <Carousel items={cbDta} />
       <p className="font-cookierun text-2xl sm:text-3xl md:text-4xl text-center">[학내 프로젝트]</p>
       <Carousel items={acData} />
     </div>
